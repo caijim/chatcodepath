05d310e957fc831455fb1a386a75065751cc1ebc
Jim <caijim@github.com>
2015-04-23 20:41:57 -0700

    first commit

---
+++ chat/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  chat
+//
+//  Created by Jim Cai on 4/23/15.
+//  Copyright (c) 2015 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
